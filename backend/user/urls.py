ba2982f7aa0a8e5fd2531f3cef57261c4487b426
whoismaruf <whoismaruf@outlook.com>
2021-05-04 15:36:41 +0600

    removed extra usera and api apps

--- backend/user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from user.views import UserView, UserDetailView
-
-
-urlpatterns = [
-    path('', UserView.as_view()),
-    path('<int:pk>/', UserDetailView.as_view())
-]
