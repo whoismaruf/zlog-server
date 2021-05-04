17691886dffdadea02f07c4460a3f8e094b84e13
whoismaruf <whoismaruf@outlook.com>
2021-05-04 15:40:45 +0600

    modified urls and configured it into root urls

--- backend/root/urls.py
+++ backend/root/urls.py
@@ -18,5 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'), name='api'),
+    path('blog/', include('blog.urls'), name='blog'),
+    path('api_auth/', include('rest_framework.urls')),
+    path('auth/', include('dj_rest_auth.urls')),
+    path('auth/register/', include('dj_rest_auth.registration.urls')),
 ]
