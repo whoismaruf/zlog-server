2dd5abfd0df7d4c4ac4c4f3993a76070102c9a99
whoismaruf <whoismaruf@outlook.com>
2021-05-04 15:39:48 +0600

    set currently logged user as author automatically

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,15 +1,25 @@
 from rest_framework import generics
 from blog.models import Blog
+from django.contrib.auth.models import User
 from blog.serializers import BlogSerializer
-from api.permissions import AuthorPermission
+from root.permissions import AuthorPermission
 
 
 class BlogView(generics.ListCreateAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 
 class BlogDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+    permission_classes = (AuthorPermission,)
+
+
+class AuthorsBlogView(generics.ListAPIView):
+    def get_queryset(self):
+        return Blog.objects.all().filter(author=self.request.user)
     serializer_class = BlogSerializer
     permission_classes = (AuthorPermission,)
