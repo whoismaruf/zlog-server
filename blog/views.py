c97b6ab303d69220358269860e972873040b4722
whoismaruf <whoismaruf@outlook.com>
2021-06-30 00:36:09 +0600

    Allowed react dev server url to CORS

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
 from rest_framework import generics
 from blog.models import Blog
-from django.contrib.auth.models import User
 from blog.serializers import BlogSerializer
 from root.permissions import AuthorPermission
 
@@ -8,6 +7,7 @@ from root.permissions import AuthorPermission
 class BlogView(generics.ListCreateAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
@@ -19,6 +19,7 @@ class BlogDetailView(generics.RetrieveUpdateDestroyAPIView):
 
 
 class AuthorsBlogView(generics.ListAPIView):
+
     def get_queryset(self):
         return Blog.objects.all().filter(author=self.request.user)
     serializer_class = BlogSerializer
--- root/settings.py
+++ root/settings.py
@@ -54,7 +54,9 @@ INSTALLED_APPS = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
-    "http://127.0.0.1:8000"
+    "http://localhost:3000",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:3000",
 ]
 CORS_ALLOW_METHODS = [
     'DELETE',
